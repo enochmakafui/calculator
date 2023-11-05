380a70f1cf09b03e68fd83450ea10fd7ef22900f
enochmakafui <108337287+enochmakafui@users.noreply.github.com>
2023-11-05 00:29:47 +0000

    input add

--- src/button.jsx
+++ src/button.jsx
@@ -1,6 +1,13 @@
+import {useState} from "react"
+
+
 const Button = (props) =>{
+    const [calText,setCalcText] = useState(" ")
     return(
-        <button className=" w-20 h-20 border-none outline-none rounded-md font-bold text-2xl shadow-lg" style={{backgroundColor:props.color,color:props.textColor}}>{props.item}</button>
+        <button className=" w-20 h-20 border-none outline-none rounded-md font-bold text-2xl shadow-lg" onClick={()=>{
+          props.showText(props.item)
+        }}
+         style={{backgroundColor:props.bgColor,color:props.textColor}}>{props.item}</button>
 
 
 
--- src/dataInput.jsx
+++ src/dataInput.jsx
@@ -1,31 +1,53 @@
-import Button from "./button"
+import {useState} from "react"
 const KEYS = ["7","8","9","DEL","4","5","6","+","1","2","3","-",".","0","/","X"]
 
 
 const DataInput = () => {
+  const [inputText,setInputText] = useState("0")
+  function handleClick(value) {
+    if (value === "DEL") {
+      // Handle deletion logic
+      setInputText(inputText.slice(0, -1));
+    } else if (value === "=") {
+      // Handle calculation logic
+      try {
+        setInputText(eval(inputText));
+      } catch (error) {
+        setInputText("Error");
+      }
+    } else if (value === "+" || value === "-" || value === "*" || value === "/") {
+      // Set the current operation to the clicked operator
+      setInputText(inputText + value);
+    } else {
+      // Append the clicked button value to the input
+      setInputText(inputText + value);
+    }
+  }
+  
 
 return(
     <div >
     <div id="calc-input" className="px-5 w-full ">
-    <input dir="rtl" type="text" className="bg-back w-96 h-20 text-4xl rounded-md outline-none" style={{caretColor: 'transparent',backgroundColor:"#EEEEEE"}} />
+    <input dir="rtl" type="text" className="bg-back w-96 h-20 text-4xl rounded-md outline-none" style={{caretColor: 'transparent',backgroundColor:"#EEEEEE"}} value={inputText} />
     </div>
     <div className=" mt-5 py-5 px-5 mx-5 grid rounded-md grid-cols-4 gap-y-4 gap-x-4 w-96 " style={{
         backgroundColor:'#D3CDCD'
     }}>
    {KEYS.map((item, index) => {
   return item === "DEL" ? (
-    <Button item={item} key={index} color="#64B4BD" textColor="white"/>
+    <button className="w-20 h-20 border-none outline-none rounded-md font-bold text-2xl shadow-lg" style={{backgroundColor:"#64B4BD",color:"white"}} onClick={()=>{handleClick(item)}}>{item} </button>
   ) : (
-    <Button item={item} key={index} color="#E5E4E0" />
-  );
+    <button className="w-20 h-20 border-none outline-none rounded-md font-bold text-2xl shadow-lg" style={{backgroundColor:"#E5E4E0"}} onClick={()=>{
+    handleClick(item)}}>{item} </button>
+    );
 })}
     <button className="text-white w-44 h-20 border-none outline-none rounded-md  font-bold text-2xl col-span-2 " style={{backgroundColor:"#64B4BD"}}>RESET</button>
     <button className="text-white w-44 h-20 border-none outline-none rounded-md font-bold text-2xl col-span-2 " style={{backgroundColor:"#FF8B38"}}>=</button>
     </div>
-    <div >
+    
 
 
-    </div>
+    
     </div>
 )
 
